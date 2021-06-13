94851c28d5498e5b4e8a19304993fdf6b389db12
= <=>
2021-06-13 16:22:49 -0500

    Added call to get options chanins from td api

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from argparse import ArgumentParser
 import requests
-import os 
+import os
+import json 
 
-if __name__ == '__main__':
-    print(f"Hello")
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("--symbol", default="SPY", help="Symbol to analyze")
+    args = p.parse_args()
+    apikey = os.environ["tdapikey"]
+    if apikey is not None:
+        print("Found API Key")
+    host = "https://api.tdameritrade.com/v1/marketdata/chains"
+    input_params = {"apikey": apikey, "symbol": args.symbol}
+
+    data = requests.get(host, params=input_params)
+    if data.status_code != 200:
+        print(f"Got error code {data.status_code} from TD")
+        os.exit(1)
+    # filter low open interest
+    
+    print(f"{json.dumps(data.json())}")
