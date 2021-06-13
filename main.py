b6624a37c81833f8173d241080d0210640885dcb
= <=>
2021-06-13 17:19:40 -0500

    Added logic to compute ITM vertical spreads where the max profit is at least 50% of the width of the strikes'

--- main.py
+++ main.py
@@ -17,9 +17,58 @@ def get_contracts(exp_map):
     return results
 
 def filter_mean_open_interest(contracts):
-    median_oi = statistics.mean([c.get('openInterest') for c in contracts])
-    contracts = [c for c in contracts if c.get('openInterest') >= median_oi]
+    mean_oi = statistics.mean([c.get('openInterest') for c in contracts])
+    contracts = [c for c in contracts if c.get('openInterest') >= mean_oi]
     return contracts
+
+def get_itm_contracts(contracts):
+    """
+    returns ITM contracts grouped by days to expiration
+    """
+    contracts_by_dte = {}
+    for c in contracts:
+        if not c["inTheMoney"]:
+            continue 
+        dte = c["daysToExpiration"]
+        if dte in contracts_by_dte:
+            contracts_by_dte[dte].append(c)
+        else:
+            contracts_by_dte[dte] = [c]
+        contracts_by_dte[dte].sort(key = lambda c: c["strikePrice"])
+    return contracts_by_dte
+
+def is_good_vertical_spread(a,b):
+    """
+    a = long leg 
+    b = short leg
+    """
+    strike_width = b["strikePrice"] - a["strikePrice"]
+    if strike_width == 0:
+        return False
+    debit_required = a["last"] - b["last"]
+    return debit_required / strike_width >= 0.5
+
+
+def compute_vertical_spreads(contracts):
+    """
+    computes reasonable vertical spreads 
+    such that they meet the following criteria:
+        - expiration date is the same date 
+        - both contracts are ITM
+        - the max profit is at least 50% of the width of the strikes 
+    """
+    grouped_contracts = get_itm_contracts(contracts)
+    spreads = {}
+    for dte, contracts in grouped_contracts.items():
+        for i in range(len(contracts)):
+            current_contract = contracts[i]
+            for j in range(i,len(contracts)):
+                compare_contract = contracts[j]
+                if is_good_vertical_spread(current_contract, compare_contract):
+                    key =  f"{current_contract.get('description')} / {compare_contract.get('description')}"
+                    spreads[key] = [current_contract, compare_contract]
+
+    return spreads
 
 if __name__ == "__main__":
     start = datetime.now()
@@ -46,8 +95,7 @@ if __name__ == "__main__":
     puts = filter_mean_open_interest(puts)
     calls = filter_mean_open_interest(calls)
 
-    print(f"{len(puts)} puts, {len(calls)} calls")
-    print(json.dumps(puts))
+    calls = compute_vertical_spreads(calls)
     print(json.dumps(calls))
     print(f"{datetime.now()-start}")
 
