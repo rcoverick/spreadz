53ecb84216c406dd11e3f60c548be947258ad70d
= <=>
2021-06-13 16:37:08 -0500

    Added functionality to filter by average open interest

--- main.py
+++ main.py
@@ -2,8 +2,27 @@ from argparse import ArgumentParser
 import requests
 import os
 import json 
+from datetime import datetime 
+import statistics
+
+def get_contracts(exp_map):
+    """
+    extracts options contracts from expiration map
+    """
+    results = []
+    for expDate, expStrikesMap in exp_map.items():
+        for strikePrice, contracts in expStrikesMap.items():
+            for contract in contracts:
+                results.append(contract)
+    return results
+
+def filter_meean_open_interest(contracts):
+    median_oi = statistics.mean([c.get('openInterest') for c in contracts])
+    contracts = [c for c in contracts if c.get('openInterest') >= median_oi]
+    return contracts
 
 if __name__ == "__main__":
+    start = datetime.now()
     p = ArgumentParser()
     p.add_argument("--symbol", default="SPY", help="Symbol to analyze")
     args = p.parse_args()
@@ -17,6 +36,16 @@ if __name__ == "__main__":
     if data.status_code != 200:
         print(f"Got error code {data.status_code} from TD")
         os.exit(1)
-    # filter low open interest
+    else:
+        data = data.json()
     
-    print(f"{json.dumps(data.json())}")
+    puts = get_contracts(data.get('putExpDateMap'))
+    calls = get_contracts(data.get('callExpDateMap'))
+    
+    print(f"{len(puts)} puts, {len(calls)} calls")
+    puts = filter_median_open_interest(puts)
+    calls = filter_median_open_interest(calls)
+    
+    print(f"{len(puts)} puts, {len(calls)} calls")
+
+    print(f"{datetime.now()-start}")
